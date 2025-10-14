e974919bacd91846d398ce3196a47535bc76532d
Cortez Jackson - DES GYE <jcortez@aduana.gob.ec>
2025-10-14 17:16:23 -0500

    pruebas

---
+++ js/sptv.js
@@ -0,0 +1,98 @@
+/*
+*
+*/
+let tv = null;
+cargarJSON();
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
+
+function makeListTV() {
+    console.log("Lista TV: Inicio");
+    const listChannel = document.getElementById("channels-group");
+    listChannel.innerHTML = "";
+
+    for (const item of tv) {
+        console.log("Lista TV: " + item.name);
+
+        const channel = document.createElement("button");
+        channel.type = "button";
+
+        const isLink = item.tipo === "LINK";
+        if (isLink) {
+            const link = document.createElement("a");
+            link.innerHTML = item.name + " &#x1f517;";
+            link.href = item.source;
+            link.rel = "noreferrer";
+            link.target = "_top";
+            listChannel.appendChild(link);
+        } else {
+            channel.innerHTML = item.name;
+            channel.id = item.name;
+            channel.addEventListener("click", () => playIframe(tv.indexOf(item)));
+            listChannel.appendChild(channel);
+        }
+    }
+    playIframe(channel);
+}
+
+// Play recurso
+function playIframe(play) {
+    console.log("Play iframe: Inicio");
+    channel = play;
+    const main = document.getElementById("mainStream");
+    const iframe = document.createElement("iframe");
+    const item = tv[play];
+
+    iframe.id = "iPlayer";
+    iframe.title = "Video Player";
+    iframe.scrolling = "no";
+    iframe.allow = "encrypted-media; autoplay; fullscreen; accelerometer 'none'; gyroscope 'none'; clipboard-write 'none'; camera 'none'; microphone 'none'; geolocation 'none'; payment 'none'";
+    iframe.allowFullscreen = "true";
+    iframe.allowtransparency = "false";
+    iframe.fetchpriority = "low";
+    iframe.loading = "lazy";
+
+    iframe.referrerPolicy = "no-referrer";
+
+    main.innerHTML = "";
+    main.appendChild(iframe);
+
+    console.log("Play iframe: " + item.tipo);
+    if (item.tipo == "IFRAME") {
+        iframe.sandbox = "allow-same-origin allow-scripts allow-presentation";
+        iframe.contentWindow.console.console.log = function () { };
+        iframe.src = item.source;
+    } else {
+        let src = "shaka.html"
+        src += "?tipo=" + item.tipo;
+        src += "&img=" + item.poster;
+        src += "&url=" + item.source;
+        if (item.tipo === "OPEN-DRM") {
+            src += "&k1=" + item.key.replace(":", "&k2=");
+        }
+        iframe.src = src;
+    }
+}
+
+window.addEventListener("load", () => {
+    console.log(tv);
+    makeListTV();
+});
+
+async function cargarJSON() {
+  try {
+    const response = await fetch('data/channels.json');
+    const data = await response.json();
+    console.log('Datos cargados:', data);
+    tv = data;
+  } catch (error) {
+    console.error('Error al cargar el JSON:', error);
+  }
+}
+
